0a60fab61b72713212a68784f1afdc65b6729ba4
Jason Hough <jasonhough0@gmail.com>
2019-02-24 10:57:52 -0600

    Updates the HistoryTableViewCell

--- Pomodoro/Pomodoro/Controllers/HistoryTableViewController.swift
+++ Pomodoro/Pomodoro/Controllers/HistoryTableViewController.swift
@@ -11,16 +11,25 @@ import UIKit
 class HistoryTableViewController: UITableViewController {
 
     var tabBarVC: TabBarViewController?
+    var distractionAverage: Int?
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
         tableView.reloadData()
+
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tabBarVC = (self.tabBarController as? TabBarViewController)!
+        var pomodoroCount = tabBarVC!.listOfPomodoros.count
+        var distractionTotal = 0
+        for pomodoro in tabBarVC!.listOfPomodoros {
+            distractionTotal += pomodoro.listOfDistractionTimes.count
+        }
+        distractionAverage = distractionTotal / pomodoroCount
+        
     }
 
     // MARK: - Table view data source
@@ -43,7 +52,8 @@ class HistoryTableViewController: UITableViewController {
         dateFormatter.locale = Locale(identifier: "en_US")
         
         cell.textLabel?.text = "\(dateFormatter.string(from: tabBarVC!.listOfPomodoros[indexPath.row].executedOnDate))"
-        cell.detailTextLabel?.text = "Number of Distractions: \(tabBarVC!.listOfPomodoros[indexPath.row].listOfDistractionTimes.count)"
+        print(distractionAverage);
+        cell.detailTextLabel?.text = "Performance: \(Double(tabBarVC!.listOfPomodoros[indexPath.row].listOfDistractionTimes.count) / Double(distractionAverage!) * 2.5)"
         
         return cell
     }
